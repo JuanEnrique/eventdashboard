e16657af55cb84d2e3c6c421c3264143714fd8df
Juan Enrique <jepcmw@gmail.com>
2025-03-05 15:32:47 +0100

    Mejorando el responsive y los colores

--- src/app/Componentes/empleados/empleados.component.ts
+++ src/app/Componentes/empleados/empleados.component.ts
@@ -11,7 +11,7 @@ import { empleado } from '../../Modelos/empleado.model';
 export class EmpleadosComponent {
 
   empleados:empleado[] = [
-    new empleado("Juan", "Pérez", "JP", "juan@example.com", "123456789", "12345678A", "123456789012", "Desarrollador", 40, "ES123456789", 1),
+    new empleado("Juan", "Pérez", "Juanito el de los palotes", "juan@example.com", "123456789", "12345678A", "123456789012", "Desarrollador", 40, "ES123456789", 1),
     new empleado("María", "López", "ML", "maria@example.com", "987654321", "87654321B", "987654321098", "Diseñadora", 35, "ES987654321", 2),
     new empleado("Carlos", "Gómez", "CG", "", "654123987", "32165487C", "654321987654", "Gerente", 45, "ES654321987", 3),
     new empleado("Laura", "Fernández", "LF", "laura@example.com", "741852963", "36985214D", "852741963852", "Contadora", 38, "ES852741963", 4),
