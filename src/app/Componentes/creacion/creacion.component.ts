a1f23af700da2c91f1f5ba69846f8c2391045e0b
Juan Enrique <jepcmw@gmail.com>
2025-03-06 19:34:28 +0100

    Formulario reactivo, no vea...

--- src/app/Componentes/creacion/creacion.component.ts
+++ src/app/Componentes/creacion/creacion.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ModalComponent } from '../modal/modal.component';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-creacion',
   standalone: true,
-  imports: [ModalComponent],
+  imports: [ModalComponent, ReactiveFormsModule],
   templateUrl: './creacion.component.html',
   styleUrl: './creacion.component.css'
 })
@@ -15,7 +15,88 @@ export class CreacionComponent {
   evento: boolean = false;
   showModal: boolean = false;
 
-  nombre: string = '';
+  
+
+
+
+
+
+
+/*_______________________________________Recogida de datos de los FORM___________________________________*/
+
+
+formulario_empleado: FormGroup;
+formulario_evento: FormGroup;
+
+constructor() {
+
+  this.formulario_empleado = new FormGroup({
+    nombre: new FormControl('',[Validators.minLength(3)]),
+    apellido: new FormControl('',[Validators.minLength(3)]),
+    alias: new FormControl('',[Validators.minLength(3)]),
+    email: new FormControl('',[Validators.minLength(3)]),
+    telefono: new FormControl('',[Validators.minLength(3)]),
+    dni:new FormControl('',[Validators.minLength(3)]),
+    nss: new FormControl('',[Validators.minLength(3)]),
+    puesto: new FormControl('',[Validators.minLength(3)]),
+    horas: new FormControl('',[Validators.minLength(3)]),
+    iban: new FormControl('',[Validators.minLength(3)]),
+    docprev: new FormControl('',[Validators.minLength(3)])
+  });
+  this.formulario_evento = new FormGroup({
+
+  });
+
+
+
+}
+
+onSubmit_empleado() {
+  if (this.formulario_empleado.valid) {
+    console.log('Formulario enviado:', this.formulario_empleado.value);
+    console.log('Formulario enviado:', this.formulario_empleado.value.nombre);
+  } else {
+    console.log('Formulario no válido');
+  }
+}
+
+onSubmit_evento() {
+  if (this.formulario_empleado.valid) {
+    console.log('Formulario enviado:', this.formulario_empleado.value);
+    console.log('Formulario enviado:', this.formulario_empleado.value.nombre);
+  } else {
+    console.log('Formulario no válido');
+  }
+}
+
+
+/*__________________________________________________________________________*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   actformempleado() {
     this.evento = false;
@@ -30,11 +111,16 @@ export class CreacionComponent {
   /*---Acciones con el modal del menú*/
   openModal() {
     this.showModal = true;
-  }
+  };
 
   closeModal() {
     this.showModal = false;
-  }
+  };
 
+  /* Prueba */
+
+  prueba(){
+    console.log("Prueba");
+  };
 
 }
